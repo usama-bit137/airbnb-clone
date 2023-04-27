66a0c080ace667b6a47e94ce3b0662c92e9d8b5f
usama-bit137 <usamaaqeel98@gmail.com>
2023-04-27 12:51:44 +0100

    Hero section added. Styles added to hero section

---
+++ src/Hero.js
@@ -0,0 +1,20 @@
+import React from "react"
+import photo from "./photo-grid.png"
+import "./Hero.css"
+export default function Hero() {
+    return (
+        <section className="container">
+            <div className="image-container">
+            <img src={photo} alt="grid" width="500px" className="photo-grid"/>
+            </div>
+            <div>
+            <h1>Online Experiences</h1>
+            <p className="hero-text">
+                Join unique interactive activities led
+                by one-of-a-kind hosts - all without 
+                leaving home.
+            </p>
+            </div>
+        </section>
+    )
+}
