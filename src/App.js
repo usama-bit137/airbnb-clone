2cb37136658913e7bdf910a686a41d8b7f97e70c
usama-bit137 <usamaaqeel98@gmail.com>
2023-04-27 13:36:33 +0100

    Card component implemented into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Card from "./Card"; 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Hero />
+        <Card />
       </header>
     </div>
   );
