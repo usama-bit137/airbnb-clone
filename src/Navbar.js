f7fffd2771ddd89f68033d0519713d43d3d341ce
usama-bit137 <usamaaqeel98@gmail.com>
2023-04-27 12:08:07 +0100

    Navbar containing logo completed. Navbar styles added

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css"
+
+import logo from "./airbnb-logo.png"
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src={logo} alt="airbnb logo" className="nav-logo" width="100px"/>
+        </nav>
+    )
+}
