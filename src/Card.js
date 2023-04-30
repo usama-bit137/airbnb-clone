ee07110575efc7ac5448daeb4fc68a2be55b636a
usama-bit137 <usamaaqeel98@gmail.com>
2023-04-30 19:54:44 +0100

    Card component altered to fit .map() method

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./Card.css"
-import star from "./216411_star_icon.png"
+import star from "./images/216411_star_icon.png"
+
 export default function Card(props) {
     return (
         <section className="card-component">
             <img />
             <div className="info-container">
-                <img src= {props.img} alt= {props.altImg} className="image" width="250px"></img>
+                <img src= {`./images/${props.img}`} alt= {props.altImg} className="image" width="250px"></img>
                 <div className="card-info">
                     <p className="rating"><img src={star} alt="star" width="20px" /> {props.rating} ({props.raters}) - {props.location}</p>
                     <p className="card-title">{props.title}</p>
-                    <p className="card-price"><span className="card-price-bold">From {props.price}</span>/person</p>
+                    <p className="card-price"><span className="card-price-bold">From ${props.price}</span>/person</p>
                 </div>
-                <div></div>
-                <div></div>
             </div>
         </section>
     )
-}
+} 
