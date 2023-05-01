69b67e22ce692f3244b7856e75f21fd92abd5aca
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-01 15:12:02 +0100

    Card component altered to accomodate the passing object through props. Card component has a conditional rendering which displays badge showing 'SOLD OUT' status for sold out events and 'ONLINE' status for online events. Added openSpots element to cards object to check if spaces are available.

--- src/Card.js
+++ src/Card.js
@@ -3,18 +3,24 @@ import "./Card.css"
 import star from "./216411_star_icon.png"
 
 export default function Card(props) {
-    console.log(props.openSpots)
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <section className="card-component">
             <div className="info-container">
-                <img src= {`./images/${props.img}`} alt= {props.altImg} className="image" width="250px"></img>
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+                <img src= {`./images/${props.item.img}`} alt= {props.item.altImg} className="image" width="250px"></img>
                 <div className="card-info">
                     <p className="rating">
-                        <img src={star} alt="star" width="20px" /> {props.stats.rating} ({props.stats.raters}) - {props.location}
+                        <img src={star} alt="star" width="20px" /> {props.item.stats.rating} ({props.item.stats.raters}) - {props.item.location}
                     </p>
-                    <p className="card-title">{props.title}</p>
+                    <p className="card-title">{props.item.title}</p>
                     <p className="card-price">
-                        <span className="card-price-bold">From ${props.price}</span>/person
+                        <span className="card-price-bold">From ${props.item.price}</span>/person
                     </p>
                 </div>
             </div>
