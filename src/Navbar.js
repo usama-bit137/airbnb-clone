056983188eea031caeb4157c6ea464009b970629
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-01 06:11:40 +0100

    import statement editted to include new directory location of the logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css"
-import logo from "./images/airbnb-logo.png"
+import logo from "./airbnb-logo.png"
 
 export default function Navbar() {
     return (
