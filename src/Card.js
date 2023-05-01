0b1bcd4a901242c28fc9fd51b13c2726f2655fa0
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-01 14:37:43 +0100

    openSpots property created in json.

--- src/Card.js
+++ src/Card.js
@@ -3,15 +3,19 @@ import "./Card.css"
 import star from "./216411_star_icon.png"
 
 export default function Card(props) {
+    console.log(props.openSpots)
     return (
         <section className="card-component">
-            <img />
             <div className="info-container">
                 <img src= {`./images/${props.img}`} alt= {props.altImg} className="image" width="250px"></img>
                 <div className="card-info">
-                    <p className="rating"><img src={star} alt="star" width="20px" /> {props.rating} ({props.raters}) - {props.location}</p>
+                    <p className="rating">
+                        <img src={star} alt="star" width="20px" /> {props.stats.rating} ({props.stats.raters}) - {props.location}
+                    </p>
                     <p className="card-title">{props.title}</p>
-                    <p className="card-price"><span className="card-price-bold">From ${props.price}</span>/person</p>
+                    <p className="card-price">
+                        <span className="card-price-bold">From ${props.price}</span>/person
+                    </p>
                 </div>
             </div>
         </section>
